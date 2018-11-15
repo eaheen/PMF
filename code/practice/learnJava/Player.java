7e0fd2f57ee1430a9c44e6862c2e149f83b9fa7d
Erik Heen <heenx008@umn.edu>
2018-11-15 15:48:00 -0600

    uhhhh nani???

---
+++ code/practice/learnJava/Player.java
@@ -0,0 +1,25 @@
+public class Player{
+    static private Float maxHealth = 100;
+    private String name;
+    private Float health;
+
+    public class Player(string){
+
+
+    }
+    void longRest(){
+        Player.health = Player.maxHealth
+    }
+
+    void shortRest(){
+
+    }
+
+    void spellCast(){
+
+    }
+
+    void rupture(){
+        
+    }
+}
